42135fcc6d0e428b956bcf45c8ae08e62c425e9a
Oskar <Oskar@DESKTOP-FIK3NL3>
2023-09-22 14:05:11 +0200

    Add project files.

---
+++ NumbersGame/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace NumbersGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Välkommen! Jag tänker på ett nummer. Kan du gissa vilket? Du får fem försök.");
+
+            Random random = new Random();
+            int numer = random.Next (1, 20);
+
+
+
+        }
+    }
+}
